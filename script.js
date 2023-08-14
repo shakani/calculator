cb34d28c2376498944179582125feb8b3647b036
Sami Hakani <sami.hakani@gmail.com>
2023-08-13 23:05:58 -0700

    wrote general operator method for calcultor

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+// calculator operate function
+function operate(a, op, b) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default: 
+            return 'ERROR';
+    }
+}
